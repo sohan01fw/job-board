ecaf0f451fd11b75dea5a26c5eafdc681c1a5cba
sohan01fw <sohancwactfw@gmail.com>
2025-10-07 14:36:18 +0545

    add node env default

--- lib/url.ts
+++ lib/url.ts
@@ -1,5 +1,15 @@
-export const SITE_URL = `https://job-board-all.vercel.app`;
-export const REDIRECT_URL = `https://job-board-all.vercel.app/auth/callback`;
+// export const SITE_URL = `https://job-board-all.vercel.app`;
+// export const REDIRECT_URL = `https://job-board-all.vercel.app/auth/callback`;
 
-export const localURL = `http://localhost:3000`;
-export const localREDIRECT_URL = `http://localhost:3000/auth/callback`;
+// export const localURL = `http://localhost:3000`;
+// export const localREDIRECT_URL = `http://localhost:3000/auth/callback`;
+
+export const SITE_URL =
+  process.env.NODE_ENV === "production"
+    ? "https://job-board-all.vercel.app"
+    : "http://localhost:3000";
+
+export const REDIRECT_URL =
+  process.env.NODE_ENV === "production"
+    ? "https://job-board-all.vercel.app/auth/callback"
+    : "http://localhost:3000/auth/callback";
