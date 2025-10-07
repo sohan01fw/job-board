ed24af7aa505af680688d824e00da24a85c383fb
sohan01fw <sohancwactfw@gmail.com>
2025-10-07 09:03:43 +0545

    add the prod redirect url

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -14,8 +14,13 @@ export async function GET(request: Request) {
     const { error } = await supabase.auth.exchangeCodeForSession(code);
     if (!error) {
       const forwardedHost = request.headers.get("x-forwarded-host"); // original origin before load balancer
-      const isLocalEnv = process.env.NODE_ENV === "development";
-      if (isLocalEnv) {
+      const redirectUrl =
+        process.env.NODE_ENV === "development"
+          ? `${origin}${next}`
+          : `https://job-board-all.vercel.app${next}`;
+
+      return NextResponse.redirect(redirectUrl);
+      if (redirectUrl) {
         // we can be sure that there is no load balancer in between, so no need to watch for X-Forwarded-Host
         return NextResponse.redirect(`${origin}${next}`);
       } else if (forwardedHost) {
--- features/auth/actions.ts
+++ features/auth/actions.ts
@@ -1,25 +1,28 @@
 import { supabase } from "@/lib/supabase/supabase_client";
 
-//supabase login through magic link
+const REDIRECT_URL = process.env.NEXT_PUBLIC_SITE_URL
+  ? `${process.env.NEXT_PUBLIC_SITE_URL}/auth/callback`
+  : "http://localhost:3000/auth/callback"; // fallback for safety
+
+// Supabase login through magic link
 export async function SupabaseMagicLinkLogin(emailValue: string | undefined) {
   const { data, error } = await supabase.auth.signInWithOtp({
     email: emailValue || "",
     options: {
-      // set this to false if you do not want the user to be automatically signed up
       shouldCreateUser: true,
-      emailRedirectTo: `http://localhost:3000/auth/callback`,
+      emailRedirectTo: REDIRECT_URL,
     },
   });
 
   return { data, error };
 }
 
-//supabase login through google link
+// Supabase login through Google
 export async function SupabaseGoogleLogin() {
   const { data, error } = await supabase.auth.signInWithOAuth({
     provider: "google",
     options: {
-      redirectTo: "http://localhost:3000/auth/callback",
+      redirectTo: REDIRECT_URL,
     },
   });
   return { data, error };
